86aa1e91fe640100c32495f39b8f1aff972eab88
varunkumarreddi <varunkumarvicky15@gmail.com>
2021-11-06 12:33:35 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const navSlide=()=>{
   const burger= document.querySelector('.burger');
   const nav= document.querySelector('.navlist');
-
+  const navList = document.querySelectorAll('.navlist li');
+  //toggle Nav
   burger.addEventListener('click',()=>{
     nav.classList.toggle('nav-active');
+
+    //Animate Links
+    navList.forEach((link,index)=>{
+      if(link.style.animation){
+        link.style.animation= '';
+      }else{
+        link.style.animation = `navListFade 0.5s ease forwards ${index / 7 +0.3}s`;
+      }
+    });
+
+    //Burger animation
+    burger.classList.toggle('toggle');
   })
+  
 }
 
 navSlide();
@@ -14,10 +28,10 @@ navSlide();
 const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
 tl.to('.text', { y: '0%', duration: 1, stagger: 0.25, delay: 2.2 })
-  .to('.slider3', { y: '100%', duration: 1 ,delay:0.8})
+  .to('.slider3', { y: '100%', duration: 0.8 ,delay:0.8})
   .to('.intro', { y: '-100%', duration: 0.8, delay:0.2 })
-  .to('.slider3', { y: '-100%', duration: 0.8 }, '-=1')
-  .to('.home',{ opacity:'1',duration:0})
+  .to('.slider3', { y: '-100%', duration: 0.6 }, '-=1')
+  .fromTo('.home',{ opacity:0 },{opacity: 1, duration:1},'-=0.5')
   .to('.landing',{y:'-100%', duration:0})
 
 
